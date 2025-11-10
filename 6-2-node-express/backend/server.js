4ec9ff862db889d3b2f2277210493a3ab9e4b084
AmjadKFUPM <s202269000@kfupm.edu.sa>
2025-11-10 09:10:19 +0300

    Finished TODO-05.

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -2,11 +2,6 @@ import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 
-const app = express();
-const PORT = 3000
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
 
 {/*write code for cors*/}
 
@@ -15,4 +10,19 @@ app.use(cors());
 {/*write code to define routes*/}
 
 
+app.get("/", (req, res) => {
+    res.send("Welcome to the Quote Generator API");
+});
+
+app.get("/api/quote", (req, res) => {
+    const quote = getRandomQuote();
+  res.json({ quote });
+});
+
 {/*write code to create server*/}
+
+const app = express();
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
